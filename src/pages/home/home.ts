d32f2906175a53ba160f74c5a78905c30eccf6df
Diego Garcia <diego.garcia@fiap.com.br>
2018-03-26 12:15:39 -0300

    First commit o/ o/

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,47 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, TextInput } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  words: string[] = [];
+  typeOfTag: string = "i";
   constructor(public navCtrl: NavController) {
 
   }
+
+  verifyKeyboardEnter(event: KeyboardEvent, input: TextInput) {
+    if (event.keyCode === 13) this.addWord(input);
+  }
+
+  addWord(input: TextInput) {
+    let wordTreated = input.value.trim();
+    if(wordTreated) this.words.push(wordTreated);
+
+    this.cleanInput(input);
+  }
+
+  cleanInput(input: TextInput) {
+    input.value = "";
+  }
+
+  removeWord(index: number) {
+    this.words.splice(index, 1);
+  }
+
+  convert(source: string) {
+    if(!this.words.length) return;
+    this.words.forEach((word) => {
+      this.executeRegex(word);
+    })
+  }
+
+  executeRegex(word: string) {
+    let regex = new RegExp(`\b${word}\b`);
+    console.log(regex);
+  }
+
 
 }
